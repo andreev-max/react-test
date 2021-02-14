c975ec3884f6404a9401f687ccb9419c5acdfe46
nAzdAc <nAzdAc15071996@mail.ru>
2021-02-14 11:41:32 +0300

    feat: some settings

--- src/App.js
+++ src/App.js
@@ -1,74 +1,71 @@
-import React, { useEffect } from 'react'
-import TodoList from './Todo/TodoList'
-import Context from './context'
-import Loader from './Loader'
-import Modal from './Modal/Modal'
+import React, { useEffect } from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context';
+import Loader from './Loader';
+import Modal from './Modal/Modal';
 
-const AddTodo = React.lazy(() => new Promise(resolve => {
+const AddTodo = React.lazy(() => new Promise((resolve) => {
   setTimeout(() => {
-    resolve(import('./Todo/AddTodo'))
-  }, 3000)
-})
-)
+    resolve(import('./Todo/AddTodo'));
+  }, 3000);
+}));
 
 function App() {
-  const [todos, setTodos] = React.useState([])
-  const [loading, setLoading] = React.useState(true)
-
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then(response => response.json())
-    .then(todos => {
-      setTimeout(() => {
-        setTodos(todos)
-        setLoading(false)
-      }, 2000)
-      
-    })
-  }, [])
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
 
   function toggleTodo(id) {
     setTodos(
-    todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    }))
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    );
   }
 
   function removeTodo(id) {
-    setTodos(todos.filter((todo) => todo.id !== id))
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
-function addTodo(title) {
-  setTodos(todos.concat([{
-    title,
-    id: Date.now(),
-    completed: false,
-  }]))
-}
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false,
+    }]));
+  }
 
   return (
     <Context.Provider value={{ removeTodo }}>
-      <div className = "wrapper">
-    
-    <h1> React Test </h1> 
-    <Modal />
-<React.Suspense fallback={<Loader />}>
-  <AddTodo onCreate={addTodo}/>
-</React.Suspense>
+      <div className="wrapper">
 
+        <h1> React Fast Course MVP </h1>
+        <Modal />
+        <React.Suspense fallback={<Loader />}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
 
-{loading && <Loader />}
-{todos.length ? <TodoList todos = {todos}onToggle = {toggleTodo}/>
-  : (loading ? null : <p>No ToDos</p>)}
-    </div>
-    
+        {loading && <Loader />}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} />
+          : (loading ? null : <p>  No ToDos</p>)}
+      </div>
+
     </Context.Provider>
-    
-  )
+
+  );
 }
 
 export default App;
--- src/Loader.js
+++ src/Loader.js
@@ -1,3 +1,3 @@
-import React from 'react'
+import React from 'react';
 
-export default () =>  <div style={{display: 'flex', justifyContent: 'center', margin: '.5 rem'}} className="lds-hourglass"></div>;
+export default () => <div style={{ display: 'flex', justifyContent: 'center', margin: '.5 rem' }} className="lds-hourglass" />;
